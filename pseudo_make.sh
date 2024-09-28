488db82786044b4a7bd3cbf62c99470c3fb9dc84
KereK DereK <kerekderek1@gmail.com>
2024-09-28 17:46:55 +0400

    [misc] change log format for pseudo_make.sh

--- pseudo_make.sh
+++ pseudo_make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-x86_64-w64-mingw32-gcc ./$1/$2.cpp -o ./$1/build/$2.exe -s -ffunction-sections -fdata-sections -Wno-write-strings -fno-exceptions -fmerge-all-constants -static-libstdc++ -static-libgcc > $2_make_log.txt
+x86_64-w64-mingw32-gcc ./$1/$2.cpp -o ./$1/build/$2.exe -s -ffunction-sections -fdata-sections -Wno-write-strings -fno-exceptions -fmerge-all-constants -static-libstdc++ -static-libgcc > $2_make.log
