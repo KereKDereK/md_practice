ffb23f2c8653a86d985726edd2d07338e0bbb18e
KereK DereK <kerekderek1@gmail.com>
2024-10-11 16:16:50 +0400

    [chapter_3 | service] add malicious service that starts reverse shell upon execution

---
+++ chapter_3/to_persist.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+
+int WINAPI WinMain(HINSTANCE hInstance, 
+                   HINSTANCE hPrevInstance, 
+                   LPSTR lpCmdLine, 
+                   int nCmdShow) {
+    MessageBoxA(NULL, "Infected!","Asasdas clan", MB_OK);
+    return 0;
+}
